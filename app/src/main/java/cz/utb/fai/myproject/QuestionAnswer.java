e3427b82585de53625fa99711eeacaf46be7aca7
Lucie Šikudová <xsikudov@fi.muni.cz>
2023-01-21 14:55:31 +0100

    feat: initial commit

---
+++ app/src/main/java/cz/utb/fai/myproject/QuestionAnswer.java
@@ -0,0 +1,22 @@
+package cz.utb.fai.myproject;
+
+public class QuestionAnswer {
+    public static String[] question = {
+            "How to say frog in Czech language?",
+            "What is the color of the year 2023?",
+            "What is the capital city of the Czech Republic?"
+    };
+
+    public static String[][] choices = {
+            {"mlok", "žába", "kočka", "hřib"},
+            {"Light Purple", "Ultimate Gray", "Viva Magenta", "Very Peri"},
+            {"Brno", "Zlín", "Prešpurk", "Praha"}
+    };
+
+    public static String[] correctAnswers = {
+            "žába",
+            "Viva Magenta",
+            "Praha"
+    };
+
+}
